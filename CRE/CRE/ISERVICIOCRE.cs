b36ccbc3d806962b30a4ea27221f0c46bf34f22b
gaboramos <gabrielramossantos@outlook.com>
2017-12-02 15:00:28 -0400

    CRE SERVICIOS
    Practica

---
+++ CRE/CRE/ISERVICIOCRE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CRE
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "ISERVICIOCRE" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface ISERVICIOCRE
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
---
+++ CRE/CRE/SERVICIOCRE.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CRE
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "SERVICIOCRE" en el código, en svc y en el archivo de configuración a la vez.
+    // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione SERVICIOCRE.svc o SERVICIOCRE.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class SERVICIOCRE : ISERVICIOCRE
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
